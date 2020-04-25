68193cb7dfaee30ef8fb84d317e2a8df2b6307b3
Vivek Narang <vivek.narang10@gmail.com>
2020-04-25 15:27:39 -0400

    First commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <span>Krama Store</span>
+    </div>
+  );
+}
+
+export default App;
